15fc290137839ab86b025d39c681cc594258455f
0xc000006b <anton223@pm.me>
2022-09-28 16:31:56 +0300

    file read and regex

--- cp_1/files.py
+++ cp_1/files.py
@@ -5,16 +5,17 @@ class files:
 	def __init__(self, file_name):
 		self.file_name = file_name
 
+
 	def read(self):
 		file = open(self.file_name)
 		t = file.read()
 		file.close()
-		t = regex.sub(r'\p{^IsCyrillic}', ' ', t)
-		print(t)
+		t = regex.sub(r'\p{^IsCyrillic}', ' ', t.lower())
+		t = regex.sub(r'\s+', ' ', t)
+		t = regex.sub(r'ё', 'е', t)
+		t = regex.sub(r'ъ', 'ь', t)
+		return t
 
 
-	def text(self):
-		pass
-
-	def text_wos(self):
-		pass
+	def read_wos(self):
+		return regex.sub(r'\s', '', self.read())
--- cp_1/main.py
+++ cp_1/main.py
@@ -10,4 +10,5 @@ from files import files
 
 if __name__ == "__main__":
 	f = files("text_short.txt")
-	f.read()
+	print(f.read())
+	print(f.read_wos())
